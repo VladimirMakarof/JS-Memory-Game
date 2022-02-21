470c8b6dac668ab36d5f8d838d66c8cdb685e3c2
VladimirMakarof <stan13.09@mail.ru>
2022-02-21 12:21:28 +0300

    feat: function randome pairs

--- js/script.js
+++ js/script.js
@@ -13,6 +13,28 @@
     result.innerHTML = '<p>Failures: <span id="fails">0</span><br>Time: <span id="timer">00:00:00</span></p>';
   }
 
+  // random pairs
+  let pairs = [];
+  const createPairs = () => {
+    let number = 1;
+    let evenFlag = 0;
+    do {
+
+      // from 1 to totalSquares
+      let index = Math.floor(Math.random() * totalSquares) + 1;
+
+      if (typeof pairs['item_' + index] === 'undefined') {
+
+        pairs['item_' + index] = number;
+
+        if ((++evenFlag) % 2 === 0) {
+          number++;
+        }
+
+      }
+
+    } while (number <= totalSquares / 2);
+  }
 
 
 
